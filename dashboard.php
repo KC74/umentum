82aebebc3b3cc808867fe90543a3a4b56336eb0e
Kenneth Chow <kennethchow74@gmail.com>
2017-10-01 03:00:28 -0700

    finished dashboard page.

---
+++ dashboard.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Template Name: Dashboard Page
+ *
+ * @package Umentum_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+            <header class="dashboard-header">
+                <p class="dashboard-day">day</p>
+                <div class="dashboard-circle">
+                    <p class="dashboard-day-number">5</p>
+                </div>
+            </header>
+            <section class="main-content">
+                <h1 class="dashboard-welcome">Welcome, Elan</h1>
+                <h2 class="dashboard-quote">"One day, or day one. Your choice."</h2>
+                
+                <div class="dashboard-cta">
+                    <div class="dashboard-rectangle"></div>
+                    <div class="dashboard-rectangle">
+                        <a href="#">let's get it!</a>
+                    </div>
+                </div>
+
+                <div class="dashboard-icons">
+                    <div class="dashboard-icon-block">
+                        <div class="dashboard-icon-wrapper">
+                            <img src="../wp-content/themes/umentum/images/comment2a.png"/>
+                        </div>
+                        <a href="#">feedback</a>
+                    </div>
+                    <div class="dashboard-icon-block">
+                        <div class="dashboard-icon-wrapper">
+                            <img src="../wp-content/themes/umentum/images/calendar3.png"/>
+                        </div>
+                        <a href="#">progress</a>
+                    </div>
+                    <div class="dashboard-icon-block">
+                        <div class="dashboard-icon-wrapper">
+                            <img src="../wp-content/themes/umentum/images/gallery.png"/>
+                        </div>
+                        <a href="#">gallery</a>
+                    </div>
+                </div>
+            </section>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+    
+<?php get_footer(); ?>
+<section class="bottom-extension">
+    <div class="bottom-ext-side"></div>
+</section>
--- header.php
+++ header.php
@@ -23,6 +23,9 @@
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<div class="header-logo">
+						<p class="header-logo-text">Logo</p>
+					</div>
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
 
